2e7dfba8b37a4ede3cb4f0eaaecd2bb81411ac66
EllenMarin <marin_ellen@hotmail.com>
2021-05-11 15:04:09 +0100

    condicoes

---
+++ Condições/composta.js
@@ -0,0 +1,9 @@
+//Composta
+var pais = "EUA"
+console.log(`Está vivendo em ${pais}!`)
+
+if(pais == "Brasil"){
+    console.log("Você é brasileiro")
+}else {
+    console.log("Você é estrangeiro")
+}
